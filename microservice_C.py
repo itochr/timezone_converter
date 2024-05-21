530a1ca7522e75952990aaa9ff26cd6c418d6525
Christine Ito <itochr@oregonstate.edu>
2024-05-20 17:11:21 -0700

    save

--- microservice_C.py
+++ microservice_C.py
@@ -1,2 +1,28 @@
 #receives a starting time and a +/- time difference value to determine the new time
 
+def newTime(start, timeDif):
+    """determines the new time after accounting for time difference, including day roll over"""
+    if start + timeDif >= 24:
+        newTime = (start + timeDif) - 24
+        newString = str('00:' + newTime + 'the next day')
+    elif start + timeDif < 0:
+        newTime = 24 - (start + timeDif)
+        newString = str(newTime + 'the previous day')
+    else:
+        newString = str(start + timeDif)
+    return newString
+
+
+def main():
+    while True:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.bind((HOST, PORT))
+            s.listen()
+            conn, addr = s.accept()
+            with conn:
+                while True:                
+                    data = conn.recv(1024).decode()                            
+                    conn.sendall(newTime(data).encode())
+
+if __name__=="__main__":
+    main()
--- microservice_D.py
+++ microservice_D.py
@@ -1,2 +1,21 @@
 #receives a starting state, ending state, start/end time, and formats it into a sentence
 #'When it is _ (time) in _ (state), it is _ (time) in _ (state)'
+
+def stringFormat(st_state, end_state, st_time, end_time):
+    """takes all the data and formats it into a string to return"""
+    sentence = ('When it is ' + st_time + ' in ' + st_state + ', it is ' + end_time + ' in ' + end_state + '.' )
+    return sentence
+
+def main():
+    while True:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.bind((HOST, PORT))
+            s.listen()
+            conn, addr = s.accept()
+            with conn:
+                while True:                
+                    data = conn.recv(1024).decode()                            
+                    conn.sendall(stringFormat(data).encode())
+
+if __name__=="__main__":
+    main()
